b4394e400e2d39dec8f97f073bc0e7f552cca381
Misael de Castro Bezerra <157545220+Misaeldecastro@users.noreply.github.com>
2024-02-17 23:38:48 -0300

    Add files via upload

---
+++ Numero maior ou igual a 10.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite um valor: "))
+num2 = int(input("Digite outro valor: "))
+soma = num1+num2
+print("resultado: ", soma)
+if soma > 10:
+    print("resultado maior que 10!")
+else:
+    print("Resultador menor que 10!")
+    
+    
